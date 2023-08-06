0b902a696b0533787b9b05df0629e6effaa359e0
unownp <antiutka@gmail.com>
2023-08-06 17:43:44 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 100, 26, 10, 99, 523 };
+            int[] array2 = array.OrderByDescending(x => x).ToArray();
+            Console.WriteLine(string.Join(", ", array2));
+        }
+    }
+}
